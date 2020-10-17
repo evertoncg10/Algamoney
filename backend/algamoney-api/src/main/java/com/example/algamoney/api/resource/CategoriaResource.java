ac93275e1a1e49cabdf928b1233003cd92664051
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-10-17 13:19:51 -0300

    Implementação para retornar 404 caso não exista a categoria.

--- backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,18 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
@@ -14,15 +21,42 @@ import com.example.algamoney.api.repository.CategoriaRepository;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
-    @Autowired
-    private CategoriaRepository categoriaRepository;
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
-    @GetMapping
-    public List<Categoria> listar() {
-        return categoriaRepository.findAll();
-    }
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
 
-    public String outro() {
-        return "OK";
-    }
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+
+		// Maneira tradicional
+//		Optional<Categoria> optionalCategoria = categoriaRepository.findById(codigo);
+//		if (optionalCategoria != null) {
+//			return ResponseEntity.ok(optionalCategoria.get());
+//		}
+//
+//		return ResponseEntity.notFound().build();
+
+		// Maneira utilizando Map
+//		return this.categoriaRepository.findById(codigo).map(categoria -> ResponseEntity.ok(categoria))
+//				.orElse(ResponseEntity.notFound().build());
+
+		// Maneira utilizando isPresent()
+		Optional<Categoria> categoria = this.categoriaRepository.findById(codigo);
+		return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+
+	}
 }
