666769f7ec193326d6e99ab3f4b297420be9edfd
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-10-23 21:20:30 -0300

    Removendo pessoa com Delete.

--- backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,5 +1,6 @@
 package com.example.algamoney.api.resource;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
@@ -9,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
@@ -30,6 +33,11 @@ public class PessoaResource {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
+	@GetMapping
+	public List<Pessoa> listar() {
+		return pessoaRepository.findAll();
+	}
+
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 
@@ -45,6 +53,22 @@ public class PessoaResource {
 
 		Optional<Pessoa> pessoa = this.pessoaRepository.findById(codigo);
 		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+
+		pessoaRepository.deleteById(codigo);
+
+		/*
+		 * Pode utilizar o método delete, porém é necessário passar o objeto da entidade
+		 * que se deseja deletar.
+		 */
+//		Pessoa pessoa = new Pessoa();
+//		pessoa.setCodigo(codigo);
+//		pessoaRepository.delete(pessoa);
 
 	}
 
