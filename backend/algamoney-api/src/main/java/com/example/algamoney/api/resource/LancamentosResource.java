b83f79868d905a41ee5d0191009cbb4791081d09
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-10-25 18:57:42 -0300

    Desafio: Cadastrando o primeiro lançamento.

--- backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentosResource.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentosResource.java
@@ -3,13 +3,21 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 
@@ -17,22 +25,29 @@ import com.example.algamoney.api.repository.LancamentoRepository;
 @RequestMapping("/lancamentos")
 public class LancamentosResource {
 
-	@Autowired
-	private LancamentoRepository lancamentoRepository;
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
 
-	@GetMapping
-	public List<Lancamento> listar() {
-		return lancamentoRepository.findAll();
-	}
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
-	@GetMapping("/{codigo}")
-	public ResponseEntity<Lancamento> buscarPeloCodigo(
-			@PathVariable Long codigo) {
-		Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
-		return lancamento.isPresent()
-				? ResponseEntity.ok(lancamento.get())
-				: ResponseEntity.notFound().build();
+    @GetMapping
+    public List<Lancamento> listar() {
+        return lancamentoRepository.findAll();
+    }
 
-	}
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
+        Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
+        return lancamento.isPresent() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build();
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamento.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+    }
 
 }
